d80f99a2adbc0337da6b5876cf33c7b841812525
Cristhian Muñoz <cristhian.munoz@epn.edu.ec>
2021-12-31 14:00:43 -0500

    Inicio, menu

--- app/src/main/java/com/munozcristhian/pholapsc/Main_Activity_Category.kt
+++ app/src/main/java/com/munozcristhian/pholapsc/Main_Activity_Category.kt
@@ -8,11 +8,18 @@ import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import com.munozcristhian.pholapsc.ui.main.SectionsPagerAdapter
 import com.munozcristhian.pholapsc.databinding.ActivityMainCategoryBinding
 
 class Main_Activity_Category : AppCompatActivity() {
 
+    private val rotateOpen: Animation by lazy {AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)}
+    private val rotateClose: Animation by lazy {AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)}
+    private val fromBottom: Animation by lazy {AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)}
+    private val toBottom: Animation by lazy {AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)}
     private lateinit var binding: ActivityMainCategoryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +31,16 @@ class Main_Activity_Category : AppCompatActivity() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
         viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
+        //val tabs: TabLayout = binding.tabs
+        //tabs.setupWithViewPager(viewPager)
+        //val fab: FloatingActionButton = binding.fab
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        //fab.setOnClickListener { view ->
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                ///.setAction("Action", null).show()
+
+        //}
+
     }
+
 }

7f9e5de439ce482f72af5e3212ba5df8bb5da9a1
Erick Mayorga <erick.mayorga@epn.edu.ec>
2021-12-30 20:34:32 -0500

    update Seleccion Impresion

---
+++ app/src/main/java/com/munozcristhian/pholapsc/SeleccionActivity.kt
@@ -0,0 +1,62 @@
+package com.munozcristhian.pholapsc
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
+import com.munozcristhian.pholapsc.databinding.SeleccionImpresionBinding
+
+class SeleccionActivity : AppCompatActivity() {
+    private lateinit var binding: SeleccionImpresionBinding
+    private lateinit var checkBoxes: MutableList<View>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.seleccion_impresion)
+        binding = SeleccionImpresionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgViewBackImpresion.setOnClickListener{
+            //val intention = Intent(this, HomeFotosActivity::class.java)
+            //startActivity(intention)
+        }
+
+
+        binding.imgViewCheck.setOnClickListener {
+            //val intention = Intent(this, HomeFotosActivity::class.java)
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setTitle("Solicitud de Impresión")
+            dialogBuilder.setMessage("Sus fotografías serán entregadas en máximo 3 días laborables")
+            dialogBuilder.setPositiveButton("Confirmar", DialogInterface.OnClickListener { _, _ ->
+                //startActivity(intention)
+                Toast.makeText(this, "Solicitud de impresión realizada con éxito.", Toast.LENGTH_LONG).show()
+            })
+            dialogBuilder.setNegativeButton("Cancelar", DialogInterface.OnClickListener { dialog, which ->
+                //startActivity(intention)
+            //pass
+            })
+            dialogBuilder.create().show()
+
+        }
+
+    }
+
+    fun onCheckboxClicked(view: View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+
+            when (view.id) {
+                null -> {
+                    if (checked) {
+                        view.toggle()
+                    } else {
+                        view.toggle()
+                    }
+                }
+            }
+        }
+    }
+}
